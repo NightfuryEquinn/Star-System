0b62f7a397344d6da24fdc69d2a34cc7e1e9044b
NightfuryEquinn <xianzyip8@gmail.com>
2023-05-17 09:38:19 +0800

    Augmented border animation

--- src/components/ui/Content.jsx
+++ src/components/ui/Content.jsx
@@ -22,7 +22,7 @@ export default function Content({ planet, view, onSetView }) {
 
   return (
     <>
-      <div className={`${ view ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0 pointer-events-none" } absolute top-0 right-0 bottom-0 left-0 bg-black ease-open duration-500 origin-center`}>
+      <div className={`${ view ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0 pointer-events-none" } absolute top-0 right-0 bottom-0 left-0 bg-black bg-opacity-75 ease-open duration-500 origin-center`}>
         <div className="p-10 md:p-20 h-full max-h-screen flex flex-col gap-y-10">
           <h2 id="glitched-title-1" className="glitched-title font-dune text-white text-4xl md:text-6xl self-center"></h2>
 
@@ -31,7 +31,7 @@ export default function Content({ planet, view, onSetView }) {
           </div>
 
           <button onClick={ onSetView } className="self-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
               <path className="fill-white" fill="currentColor" d="M6 19h3v-6h6v6h3v-9l-6-4.5L6 10v9Zm0 2q-.825 0-1.413-.588T4 19v-9q0-.475.213-.9t.587-.7l6-4.5q.275-.2.575-.3T12 3.5q.325 0 .625.1t.575.3l6 4.5q.375.275.588.7T20 10v9q0 .825-.588 1.413T18 21h-5v-6h-2v6H6Zm6-8.75Z"/>
             </svg>
           </button>
--- src/components/ui/SkillChart.jsx
+++ src/components/ui/SkillChart.jsx
@@ -48,7 +48,7 @@ export default function SkillChart({ thePlanetData, view }) {
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
                         <path className="fill-darkblue" fill="currentColor" d="m12 21l-1.45-1.3q-2.525-2.275-4.175-3.925T3.75 12.812Q2.775 11.5 2.388 10.4T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.3 0 2.475.55T12 4.75q.85-1 2.025-1.55t2.475-.55q2.35 0 3.925 1.575T22 8.15q0 1.15-.388 2.25t-1.362 2.412q-.975 1.313-2.625 2.963T13.45 19.7L12 21Z"/>
                       </svg>
-                      <p>Fond</p>
+                      <p className="mb-1">Fondness</p>
                     </div>
 
                     <div className={`w-full text-white bg-black bg-opacity-50 text-center rounded-xl ${ view && 'animate-fade-left'} animate-delay-[0.4s]`}>
@@ -61,7 +61,7 @@ export default function SkillChart({ thePlanetData, view }) {
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256">
                         <path className="fill-darkblue" fill="currentColor" d="M207 50.25A87.46 87.46 0 0 0 144.6 24h-.33A87.48 87.48 0 0 0 82 49.81L20.61 112a16 16 0 0 0 .06 22.56l28.66 28.66a15.92 15.92 0 0 0 11.32 4.69h.09a16 16 0 0 0 11.36-4.82l60.9-62.4a16.08 16.08 0 0 1 22.41-.21a15.6 15.6 0 0 1 4.73 11.19a16.89 16.89 0 0 1-4.85 12L93 183.88a16 16 0 0 0-.17 22.79l28.66 28.66a16.06 16.06 0 0 0 22.52.12l61.8-60.45c34.45-34.5 34.98-90.44 1.19-124.75ZM60.65 151.89L32 123.24l29.42-29.81l28.48 28.48ZM132.79 224l-28.68-28.65l30.13-29.13l28.49 28.48Z"/>
                       </svg>
-                      <p>Attract</p>
+                      <p className="mb-1">Magnetic</p>
                     </div>
 
                     <div className={`w-full text-white bg-black bg-opacity-50 text-center rounded-xl ${ view && 'animate-fade-left'} animate-delay-[0.8s]`}>
@@ -71,10 +71,10 @@ export default function SkillChart({ thePlanetData, view }) {
 
                   <div className="mb-5 flex flex-col gap-y-3">
                     <div className={`flex flex-row gap-x-2 items-center ${ view && 'animate-fade-left'} animate-delay-[1s]`}>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-                        <path className="fill-darkblue" fill="currentColor" d="M12 22q-.825 0-1.413-.588T10 20h4q0 .825-.588 1.413T12 22Zm-4-3v-2h8v2H8Zm.25-3q-1.725-1.025-2.738-2.75T4.5 9.5q0-3.125 2.188-5.313T12 2q3.125 0 5.313 2.188T19.5 9.5q0 2.025-1.012 3.75T15.75 16h-7.5Z"/>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256">
+                        <path className="fill-darkblue" fill="currentColor" d="m231.65 194.55l-33.19-157.8a16 16 0 0 0-19-12.39l-46.81 10.06a16.08 16.08 0 0 0-12.3 19l33.19 157.8A16 16 0 0 0 169.16 224a16.25 16.25 0 0 0 3.38-.36l46.81-10.06a16.09 16.09 0 0 0 12.3-19.03ZM136 50.15v-.09l46.8-10l3.33 15.87L139.33 66Zm6.62 31.47l46.82-10.05l3.34 15.9L146 97.53Zm6.64 31.57l46.82-10.06l13.3 63.24l-46.82 10.06ZM216 197.94l-46.8 10l-3.33-15.87l46.8-10.07l3.33 15.85v.09ZM104 32H56a16 16 0 0 0-16 16v160a16 16 0 0 0 16 16h48a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16ZM56 48h48v16H56Zm0 32h48v96H56Zm48 128H56v-16h48v16Z"/>
                       </svg>
-                      <p>Mastery</p>
+                      <p className="mb-1">Mastery</p>
                     </div>
 
                     <div className={`w-full text-white bg-black bg-opacity-50 text-center rounded-xl ${ view && 'animate-fade-left'} animate-delay-[1.2s]`}>
