b60d36bb68be79598de8b01f098efd1f27c8558d
NightfuryEquinn <xianzyip8@gmail.com>
2023-09-23 11:40:05 +0800

    Fixed performance leaks issue

--- src/components/blocks/F2.jsx
+++ src/components/blocks/F2.jsx
@@ -1,8 +1,6 @@
 import DissolveMaterial from "../../material/DissolveMaterial"
 
 import { animated } from "@react-spring/three"
-import { Html } from "@react-three/drei"
-import { write } from "glitched-writer"
 import { useState, useRef } from "react"
 
 export default function F2( { controls, position, scale, geometry, material } ) {
@@ -36,13 +34,11 @@ export default function F2( { controls, position, scale, geometry, material } ) 
         e.stopPropagation()
       }}
       onPointerEnter={ ( e ) => {
-        write( "F2Here", "#glitched-f2", "typewriter" )
         setHover( true )
         setVisible( false )
         e.stopPropagation()
       }} 
       onPointerLeave={ ( e ) => {
-        write( "", "#glitched-f2", "typewriter" )
         setHover( false )
         setVisible( true )
         e.stopPropagation()
@@ -52,31 +48,6 @@ export default function F2( { controls, position, scale, geometry, material } ) 
         baseMaterial={ material }
         visible={ visible }
       />
-      
-      <Html 
-        position={ [ -19, -4, 0 ] }
-        distanceFactor={ 75 }
-        center
-      >
-        <h2 
-          data-gw-string="Experimental" 
-          className={`glitched-title lg:block hidden ease-in-out duration-300 ${ hover ? "opacity-100" : "opacity-0" }`}
-        >
-          Experimental
-        </h2>
-      </Html>
     </animated.mesh>
-
-    <Html
-      position={ [ 3, -40, 0 ] }
-      distanceFactor={ 75 }
-      center
-    >
-      <h2 
-        className={`glitched-title text-center lg:hidden ease-in-out duration-300 ${ hover ? "opacity-100" : "opacity-0" }`}
-        id="glitched-f2" 
-      >
-      </h2>
-    </Html>
   </>
 }
--- src/components/blocks/Y1.jsx
+++ src/components/blocks/Y1.jsx
@@ -2,8 +2,6 @@ import DissolveMaterial from "../../material/DissolveMaterial"
 
 import { animated } from "@react-spring/three"
 import { useState, useRef } from "react"
-import { Html } from "@react-three/drei"
-import { write } from "glitched-writer"
 
 export default function Y1( { controls, position, scale, geometry, material } ) {
   const [ visible, setVisible ] = useState( true )
@@ -36,13 +34,11 @@ export default function Y1( { controls, position, scale, geometry, material } ) 
         e.stopPropagation()
       }}
       onPointerEnter={ ( e ) => {
-        write( "Y1Here", "#glitched-y1", "typewriter" )
         setHover( true )
         setVisible( false )
         e.stopPropagation()
       }} 
       onPointerLeave={ ( e ) => {
-        write( "", "#glitched-y1", "typewriter" )
         setHover( false )
         setVisible( true )
         e.stopPropagation()
@@ -52,31 +48,6 @@ export default function Y1( { controls, position, scale, geometry, material } ) 
         baseMaterial={ material }
         visible={ visible }
       />
-
-      <Html 
-        position={ [ -4, 18, 0 ] }
-        distanceFactor={ 75 }
-        center
-      >
-        <h2 
-          data-gw-string="Experimental" 
-          className={`glitched-title lg:block hidden ease-in-out duration-300 ${ hover ? "opacity-100" : "opacity-0" }`}
-        >
-          Experimental
-        </h2>
-      </Html>
     </animated.mesh>
-
-    <Html
-      position={ [ 3, -40, 0 ] }
-      distanceFactor={ 75 }
-      center
-    >
-      <h2 
-        className={`glitched-title text-center lg:hidden ease-in-out duration-300 ${ hover ? "opacity-100" : "opacity-0" }`}
-        id="glitched-y1" 
-      >
-      </h2>
-    </Html>
   </>
 }
