bac085ba8c2afe6af75a83ac70c7fed143237ddb
NightfuryEquinn <xianzyip8@gmail.com>
2023-09-27 11:46:33 +0800

    Testing on mobile

--- src/components/ui/terminal/MainTerminal.jsx
+++ src/components/ui/terminal/MainTerminal.jsx
@@ -1,36 +1,61 @@
 import { Animator } from '@arwes/react-animator';
 import { Animated } from '@arwes/react-animated';
 import { useEffect, useState } from "react";
+import { Dots, GridLines, MovingLines } from '@arwes/react';
 
 const Item = () => {
-  return(
-    <Animator>
-      <Animated
-        style={{
-          margin: 10,
-          width: 40,
-          height: 20,
-          backgroundColor: "#dee2e6"
-        }}
-        animated={{
-          initialStyle: {
-            x: 0,
-            backgroundColor: "'#ed0101"
+  return <Animator>
+    <Animated
+      style={{
+        margin: 10,
+        width: 40,
+        height: 20
+      }}
+      animated={{
+        transitions: {
+          entering: {
+            x: [ 0, 100 ],
+            backgroundColor: "#ed0101"
           },
-          transitions: {
-            entering: {
-              x: [ 0, 100 ],
-              backgroundColor: "#dee2e6"
-            },
-            exiting: {
-              x: [ 100, 0 ],
-              backgroundColor: "#0c44ac"
-            }
+          entered: {
+            x: 100,
+            backgroundColor: "#dee2e6"
+          },
+          exiting: {
+            x: [ 100, 0 ],
+            backgroundColor: "#0c44ac"
           }
-        }}
+        }
+      }}
+    />
+  </Animator>
+}
+
+const Background = () => {
+  return <Animator duration={{ interval: 10 }}>
+    <div style={{
+      position: "absolute",
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      backgroundColor: "#161a1d"
+    }}>
+      <GridLines
+        lineColor='#dee2e611'
+        distance={ 50 }
       />
-    </Animator>
-  )
+      <Dots 
+        color='#dee2e622'
+        distance={ 50 }
+      />
+      <MovingLines 
+        lineColor='#dee2e633'
+        sets={ 10 }
+        distance={ 50 }
+      />
+    </div>
+  </Animator>
 }
 
 export default function MainTerminal() {
@@ -42,9 +67,12 @@ export default function MainTerminal() {
     return () => clearInterval( tid )
   }, [])
   
-  return (
+  return <>
+    <Background />
+
     <Animator active={ active } manager="stagger" combine>
       { Array( 10 ).fill( 0 ).map( ( _, i ) => <Item key={ i } /> ) }
     </Animator>
-  )
+
+  </>
 }
