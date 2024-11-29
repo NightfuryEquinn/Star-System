0a03c087b6d6c48a993c97b25d3b8cc5d99ccae8
NightfuryEquinn <xianzyip8@gmail.com>
2024-11-29 17:03:14 +0800

    Created infinite star field

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,32 +1,13 @@
-import { Environment, OrbitControls, Stars } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
+import { Environment, OrbitControls } from '@react-three/drei'
 import { Bloom, EffectComposer, Glitch } from '@react-three/postprocessing'
 import { Perf } from "r3f-perf"
-import { useRef, useState } from 'react'
 import * as THREE from "three"
 import Earth from './components/Earth'
+import InfiniteStarField from './components/InfiniteStarField'
 import Star from './components/Star'
 
 export default function Experience() {
   const sunDirection = new THREE.Vector3()
-
-  const maxRadius = 1250
-  const orbitControlsRef = useRef<any>()
-  const [ controlsActive, setControlsActive ] = useState(true)
-
-  const checkRadius = (camera: any) => {
-    const distanceFromOrigin = camera.position.length()
-    if (distanceFromOrigin >= maxRadius) {
-      setControlsActive(false)
-    }
-  }
-
-  useFrame(() => {
-    if (!controlsActive) {
-      orbitControlsRef.current.reset()
-      setControlsActive(true)
-    }
-  })
 
   return <>
     <Perf position='top-left' />
@@ -59,21 +40,16 @@ export default function Experience() {
     <Star sunDirection={ sunDirection } />
     <Earth sunDirection={ sunDirection } />
 
-    <ambientLight intensity={ 0.5 } />
+    <InfiniteStarField count={ 100 } size={ 400 } gridSize={ 3 } />
+    <InfiniteStarField count={ 200 } size={ 800 } gridSize={ 3 } />
+
+    <ambientLight intensity={ 0.375 } />
     <pointLight castShadow intensity={ 5 } position={[0, 0, 0]}  />
 
-    <Stars radius={ 300 } depth={ 40 } count={ 2500 } factor={ 5 } saturation={ 1 } speed={ 1 } fade />
-    <Stars radius={ 400 } depth={ 40 } count={ 3750 } factor={ 7 } saturation={ 1 } speed={ 3 } fade />
-    <Stars radius={ 500 } depth={ 40 } count={ 5000 } factor={ 9 } saturation={ 1 } speed={ 5 } fade />
-
     <OrbitControls
-      ref={ orbitControlsRef }
       enableDamping
       maxDistance={ 1250 }
       panSpeed={ 1 }
-      onChange={(e: any) => {
-        checkRadius(e.target.object)
-      }}
     />
   </>
 }
---
+++ src/components/InfiniteStarField.tsx
@@ -0,0 +1,66 @@
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+import * as THREE from "three"
+
+const generateStars = (count: number, size: number) => {
+  const positions = []
+
+  for (let i = 0; i < count; i++) {
+    positions.push(
+      (Math.random() - 0.5) * size,
+      (Math.random() - 0.5) * size,
+      (Math.random() - 0.5) * size
+    )
+  }
+
+  return new Float32Array(positions)
+}
+
+export default function InfiniteStarField({ count, size, gridSize }: any) {
+  const groupRef = useRef<any>(null)
+  const positions = generateStars(count, size)
+
+  useFrame(({ camera }) => {
+    const cameraGridX = Math.floor(camera.position.x / size)
+    const cameraGridY = Math.floor(camera.position.y / size)
+    const cameraGridZ = Math.floor(camera.position.z / size)
+
+    groupRef.current.position.set(
+      cameraGridX * size,
+      cameraGridY * size,
+      cameraGridZ * size
+    );
+  });
+
+  return <>
+    <group ref={ groupRef }>
+      {Array.from({ length: gridSize ** 3 }).map((_, i) => {
+        const x = (i % gridSize) - Math.floor(gridSize / 2)
+        const y = (Math.floor(i / gridSize) % gridSize) - Math.floor(gridSize / 2)
+        const z = Math.floor(i / (gridSize * gridSize)) - Math.floor(gridSize / 2)
+
+        return (
+          <points key={ i } position={ [x * size, y * size, z * size] }>
+            <bufferGeometry>
+              <bufferAttribute
+                attach="attributes-position"
+                array={ positions }
+                count={ positions.length / 3 }
+                itemSize={ 3 }              
+              >
+              </bufferAttribute>
+            </bufferGeometry>
+            <pointsMaterial
+              size={ 0.05 }
+              sizeAttenuation
+              color="#DEE2E6"
+              transparent={ true }
+              blending={ THREE.AdditiveBlending }
+              depthWrite={ false }
+            />
+          </points>
+        )
+      })}
+    </group>
+  </>
+}
